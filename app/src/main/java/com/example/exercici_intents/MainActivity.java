4837dac12014b08b58c9020ce34bc8f3318e8d9c
Silvia Gomera Boné <silviagomera@gmail.com>
2019-06-04 10:03:56 +0200

    Permission Camera

--- app/src/main/java/com/example/exercici_intents/MainActivity.java
+++ app/src/main/java/com/example/exercici_intents/MainActivity.java
@@ -1,13 +1,81 @@
 package com.example.exercici_intents;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSION_WRITE = 1001;
+    private static final int REQUEST_PERMISSION_CAMERA = 2000;
+    private boolean permissionGranted;
+    private Button takePictureButton;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        takePictureButton = (Button) findViewById(R.id.button_image);
+        imageView = (ImageView) findViewById(R.id.imageview);
+
+        if (!permissionGranted) {
+            checkPermissions();
+            return;
+        }
+    }
+
+    // Checks if external storage is available for read and write
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    // Initiate check for permissions.
+    private boolean checkPermissions() {
+
+        //stores in permissionCheck, if the write external storage is granted or not. It's an integer
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA);
+
+        //evaluates if the value of permissionCheck is not permission_Granted. If it's not granted,
+        //then it requests the camera permision and returns false to the boolean method.
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    REQUEST_PERMISSION_CAMERA);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Handle permissions result
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CAMERA:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted = true;
+                    Toast.makeText(this, "Camera permission granted",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Take picture button disabled", Toast.LENGTH_SHORT).show();
+                    takePictureButton.setEnabled(false);
+                }
+                break;
+        }
     }
 }
